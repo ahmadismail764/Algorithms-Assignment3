ba0023d444ddfe9cd10a85ce65a48011dd3cf792
ahmadismail764 <ahmedeldeehy30305@gmail.com>
2024-12-31 08:05:11 +0200

    Greedy Question 5 is getting a wrong answer

--- Part2-Greedy Algorithms and MST/5-ShortestPath.cpp
+++ Part2-Greedy Algorithms and MST/5-ShortestPath.cpp
@@ -1,53 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-const int INF = INT_MAX;
-
 int main()
 {
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> dist(n, vector<int>(n, INF));
-
+    vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
     for (int i = 0; i < n; ++i)
         dist[i][i] = 0;
-
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; ++i)
     {
         int a, b, c;
         cin >> a >> b >> c;
-        a--;
-        b--;
-        int weight = pow(2, c);
-        dist[a][b] = weight;
-        dist[b][a] = weight;
+        --a;
+        --b;
+        dist[a][b] = min(dist[a][b], c);
+        dist[b][a] = min(dist[b][a], c);
     }
-
-    for (int k = 0; k < n; ++k)
-    {
-        // k is the intermediate vertex we are considering
-        for (int i = 0; i < n; ++i)
-        {
-            // i is the starting vertex
-            for (int j = 0; j < n; ++j)
-            {
-                // j is the ending vertex
-                if (dist[i][k] != INF && dist[k][j] != INF)
-                {
-                    // If there is a path from i to k and from k to j
+    for (int k = 1; k < n; ++k)
+        for (int i = 1; i < n; ++i)
+            for (int j = 1; j < n; ++j)
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX)
                     dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-                    // Update the shortest path from i to j to be the minimum of the current value
-                    // and the path that goes through k
-                }
-            }
-        }
-    }
-
     int answer = 0;
-    for (int i = 0; i < n; ++i)
+    for (int i = 1; i < n; ++i)
         for (int j = i + 1; j < n; ++j)
             answer += dist[i][j];
     string bitString = bitset<32>(answer).to_string();
-    size_t firstNonZero = bitString.find_first_not_of('0');
+    int firstNonZero = bitString.find_first_not_of('0');
     cout << bitString.substr(firstNonZero) << '\n';
 }
